9d13351c4bdcd10d41c8dac9b8fb7365e1a45a3a
Leandro Silva <leandroasilva290@gmail.com>
2018-11-19 12:16:40 -0200

    QR Code

---
+++ src/Codigos Visao/QR Code (QR)/Versao_2/CMakeFiles/Painel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Painel.dir/code.cpp.o"
+  "Painel.pdb"
+  "Painel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Painel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/Codigos Visao/QR Code (QR)/Versao_2/CMakeFiles/qr_code.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qr_code.dir/main.cpp.o"
+  "qr_code.pdb"
+  "qr_code"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qr_code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
