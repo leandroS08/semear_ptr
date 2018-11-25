953835e13570e6065f5625c401515fdf1e7a8172
gonzalesmk <judcnegri@gmail.com>
2018-11-25 11:29:03 -0200

    commit

--- src/path_planning.py
+++ src/path_planning.py
@@ -5,20 +5,36 @@ from semear_ptr.srv import *
 from scipy.sparse.csgraph import dijkstra, csgraph_from_dense
 import numpy as np
 
+""" Function definition:
+    start: the number of the vertice to start from
+    end: the number of the vertice to go to
+    impossible_paths: 1D matrix that store the paths that should not be taken in to account
+            The matrix should be in the form [ X1, Y1, X2, Y2, ..., Xn, Yn], where Xi and Yi are the
+            two vertices that should be disconnected
+"""
 def handle_find_path(req):
     
     start = req.start
     end = req.end
     impossible_paths= []
+    
+    # Converts the matrix [ X1, Y1, X2, Y2, ..., Xn, Yn] in [ [X1, Y1], [X2, Y2], ... ,[Xn Yn ]]
     for i,j in zip(req.impossible_paths[0::2], req.impossible_paths[1::2] ):
         impossible_paths.append([i, j])
     
-    G2_data = np.array(
-    [ 
-        [np.inf , 1     , 1], 
-        [1      , np.inf, 1], 
-        [1      , 1     , np.inf]
-    ])
+    # Matrix of cost
+    G2_data = np.ones(shape=(12,12))
+
+    for i in range(12):
+        for j in range(12):
+            is_upper =  j == i + 3
+            is_bottom = j == i - 3
+            is_left =   j == i + 1 and not (i%3 == 0)
+            is_right =  j == i - 1 and not (i%3 == 2)
+            
+            if (not is_upper) and (not is_bottom) and (not is_left) and (not is_right):
+                G2_data[i,j] = np.inf
+                G2_Data[j,i] = np.inf
 
     for (i,j) in impossible_paths:
         G2_data[i, j] = np.inf
@@ -44,6 +60,7 @@ def handle_find_path(req):
     path.pop()
     path.reverse()
     print(path)
+    
     return FindPathResponse(path)
 
 def find_path_server():
